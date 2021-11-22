5cea002dbf965c69590a60f18fa46a552fed90ec
Levi Mendes <levi.mendes@foursys.com.br>
2021-11-22 20:23:38 -0300

    tela de cadastro de quarto

---
+++ app/src/main/java/com/example/hotelmontain/ListaFuncionariosActivity.java
@@ -0,0 +1,14 @@
+package com.example.hotelmontain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaFuncionariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_funcionarios);
+    }
+}
