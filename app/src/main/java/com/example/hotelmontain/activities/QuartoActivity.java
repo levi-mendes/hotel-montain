f6d61297210c2f8b14e5203605400c7db5cf7fd3
Levi Mendes <levi.mendes@foursys.com.br>
2021-11-22 20:09:22 -0300

    tela de cadastro de quarto

--- app/src/main/java/com/example/hotelmontain/activities/QuartoActivity.java
+++ app/src/main/java/com/example/hotelmontain/activities/QuartoActivity.java
@@ -10,5 +10,8 @@ public class QuartoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quarto);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setTitle("Quarto");
     }
 }
